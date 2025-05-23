4c8f248ca075a3f4e38a7e1dcadc1384a7486df9
benjaiu10 <166828214+benjaiu10@users.noreply.github.com>
2025-05-23 10:40:48 -0300

    .

---
+++ Assets/Scripts/DeteccionDeColisiones.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+            if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
