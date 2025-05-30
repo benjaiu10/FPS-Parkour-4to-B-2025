a58e408170962b9c6db461fd48b4ce9950549f09
benjaiu10 <166828214+benjaiu10@users.noreply.github.com>
2025-05-30 10:40:37 -0300

    Money manager y los demas

---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+
+    public void UpdateMoney(float amount)
+    {
+        if (money + amount < 0)
+        {
+            //impedir la compra
+            Debug.Log("Dinero");
+        }
+        else
+        {
+            money += amount;
+        }
+    }
+    
+}
